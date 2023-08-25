524b5b65c6adab49c30cee7ab23b997775531815
teklumezgebo <mezge002@umn.edu>
2023-08-25 14:35:03 -0500

    commit tree 1a6a19b490d95e494423e40edff97e0faa8f9573

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,52 @@
 # your code here
+class MySet
+
+    attr_accessor :hash
+
+    def initialize(enumerable = [])
+        @hash = {}
+        enumerable.each do |value|
+            @hash[value] = true
+        end
+    end
+
+    def include?(value)
+        @hash.has_key?(value)
+    end
+
+    def add(value)
+        @hash[value] = true
+        self
+    end
+
+    def delete(value)
+        @hash.delete(value)
+        self
+    end
+
+    def size
+        @hash.size
+    end
+
+    def self.[](*values)
+        self.new(values)
+    end
+    
+    def clear
+        @hash.clear
+        self
+    end
+
+    def each
+        @hash.keys.each do |key|
+            yield(key)
+        end
+        self
+    end
+
+    def inspect
+        my_hash = "{" + hash.keys.join(", ") + "}"
+        "#<MySet: #{my_hash}>"
+    end
+
+end
